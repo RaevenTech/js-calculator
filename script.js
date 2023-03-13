80675fd55f0c98e805e17a220ea6ec38898deb0b
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-03-13 15:52:55 +0000

    incomplete

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 // calculator application
-console.log("hello world")
+let currentValue = ""
+let previousValue = ""
+let operator = ""
+
+document.addEventListener("DOMContentLoaded", function(e) {
+  const clearBtn = document.querySelector(".clear-btn")
+  const equalBtn = document.querySelector(".equal-sign")
+  const decimalBtn = document.querySelector(".decimal-point")
+  const numbersBtn = document.querySelectorAll(".number")
+  const opertatorsBtn = document.querySelectorAll(".operator")
+  const previousScreen = document.querySelector("#previous-input")
+  const currentScreen = document.querySelector("#current-input")
+
+  numbersBtn.forEach((number) => number.addEventListener("click", function(e){
+    handleNumber(e.target.textContent)
+  }))
+})
+
+function handleNumber(num){
+  console.log(num)
+}
